8e200961be22a34192db9234cc9285c018d40a7d
VictoriaMS <vikamag48@gmail.com>
2022-01-14 15:43:32 +0600

    add first and last stations for tickets

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -2,6 +2,8 @@ class RailwayStation < ActiveRecord::Base
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
   has_many :trains, foreign_key: :current_station_id
+  has_many :tickets, foreign_key: :first_station_id
+  has_many :tickets, foreign_key: :last_station_id
 
   validates :title, presence: true 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_161503) do
+ActiveRecord::Schema.define(version: 2022_01_14_093155) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 2022_01_13_161503) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "train_id"
+    t.integer "first_station_id"
+    t.integer "last_station_id"
+    t.index ["first_station_id"], name: "index_tickets_on_first_station_id"
+    t.index ["last_station_id"], name: "index_tickets_on_last_station_id"
     t.index ["train_id"], name: "index_tickets_on_train_id"
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
