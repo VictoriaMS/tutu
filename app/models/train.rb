18374ea5beec28eb772ab533e02d1896d0651ddc
VictoriaMS <vikamag48@gmail.com>
2022-02-04 01:51:39 +0600

    Разрешены все конфликты после слияния ветки views

--- app/models/train.rb
+++ app/models/train.rb
@@ -4,4 +4,28 @@ class Train < ActiveRecord::Base
 
   has_many :tickets
   has_many :wagons
+
+  def compartment_wagons 
+    self.wagons.select {|wagon| wagon.type_of_wagon == 'compartment' }
+  end
+
+  def econom_class_wagons
+    self.wagons.select {|wagon| wagon.type_of_wagon == 'econom-class' }
+  end
+
+  def count_top_places(type)
+    if type == 'compartment'
+      self.compartment_wagons.inject(0) {|places, wagon| places += wagon.top_places }
+    else 
+      self.econom_class_wagons.inject(0) {|places, wagon| places += wagon.top_places }
+    end
+  end
+
+  def count_bottom_places(type)
+    if type == 'compartment'
+      self.compartment_wagons.inject(0) {|places, wagon| places += wagon.bottom_places }
+    else 
+      self.econom_class_wagons.inject(0) {|places, wagon| places += wagon.bottom_places }
+    end
+  end
 end
